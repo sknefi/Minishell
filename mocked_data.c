b5fd5173eff6c3b45cbcba1a61cd13749c7f704e
sknefi <filip.karika1@gmail.com>
2025-03-02 23:40:07 +0100

    updated echo exec, now working with -n, -n...n flag

--- mocked_data.c
+++ mocked_data.c
@@ -8,10 +8,12 @@ t_token	*mocked_token_echo(void)
 	
 	token = NULL;
 	token = token_append(token, "echo", TOKEN_COMMAND);
-	token_append(token, "Hello ", TOKEN_WORD_NVAR);
+	token_append(token, "-n", TOKEN_ARG);
+	token_append(token, "-nnnnn", TOKEN_ARG);
+	token_append(token, "Hello", TOKEN_WORD_NVAR);
 	token_append(token, "$USER", TOKEN_WORD_VAR);
 	token_append(token, "!", TOKEN_WORD_NVAR);
-	token_append(token, " How is ur day?", TOKEN_WORD_NVAR);
+	token_append(token, "How is ur day?", TOKEN_WORD_NVAR);
 	return (token);
 }
 
--- src/builtins/sh_echo.c
+++ src/builtins/sh_echo.c
@@ -1,17 +1,52 @@
 #include "../../include/minishell.h"
 
+static int	is_n_flag(char *arg)
+{
+	size_t	i;
+	size_t	arg_len;
+
+	arg_len = strlen(arg);
+	if (arg_len <= 1 || arg[0] != '-')
+		return (false);
+	i = 1;
+	while (i < arg_len)
+	{
+		if (arg[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);	
+}
+
 // Function to print the arguments passed to echo
 int	sh_echo(t_app *app, t_token *token)
 {
+	int		n_flag;
+
+	n_flag = false;
 	token = token->next;
+
+	// handling only -n or -nnn or -nnnnn... options
+	while (token && token->type == TOKEN_ARG)
+	{
+		if (!is_n_flag(token->data))
+			break ;
+		n_flag = true;
+		token = token->next;
+	}
+	
 	while (token)
 	{
 		if (token->type == TOKEN_WORD_NVAR)
 			p("%s", token->data);
 		else if (token->type == TOKEN_WORD_VAR)
-			p("%s", get_env_var(token->data + 1, app->env)); // + 1 to skip the '$' character
+			p("%s", get_env_var(token->data + 1, app->env)); // +1 to skip the '$' character
+		if (token->next) // dont print space after the last argument
+			p("_");
 		token = token->next;
 	}
+	if (!n_flag)
+		p("\n");
 	app->exit_status = 0;
 	return (0);
 }
