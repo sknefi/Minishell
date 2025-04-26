937665218af0298192f981237b675baf53f25f1a
T <tmateja@c2r9s2.42prague.com>
2025-04-26 16:09:31 +0200

    SIGINT done

--- source/signals.c
+++ source/signals.c
@@ -26,8 +26,10 @@ static void	sigint_handler(int sig)
 {
 	(void)sig;
 
-	write(STDOUT_FILENO, "^C\n", 3);
-	exit(EXIT_SUCCESS);
+	write(STDOUT_FILENO, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 }
 
 static void	sigquit_handler(int sig)
