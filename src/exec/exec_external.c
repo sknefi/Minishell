507505c1655dc602a6d4d4bc5fed34efbaf45eeb
sknefi <filip.karika1@gmail.com>
2025-05-02 14:34:30 +0200

    fixed leaks in fork()

--- src/exec/exec_external.c
+++ src/exec/exec_external.c
@@ -12,18 +12,29 @@ static char	*get_cmd_path(t_app *app, char **cmd_args)
 	if (!path)
 		return (NULL);
 	all_paths = ft_split(path, ':');
+	if (!all_paths)
+		return (NULL);
 	i = 0;
 	while (all_paths[i])
 	{
 		cmd_path = ft_strjoin(all_paths[i], "/");
 		if (!cmd_path)
-			return (free_dpp(all_paths), NULL);
+		{
+			free_dpp(all_paths);
+			return (NULL);
+		}
 		tmp = ft_strjoin(cmd_path, cmd_args[0]);
 		free(cmd_path);
 		if (!tmp)
-			return (free_dpp(all_paths), NULL);
+		{
+			free_dpp(all_paths);
+			return (NULL);
+		}
 		if (access(tmp, F_OK) == 0)
-			return (free_dpp(all_paths), tmp);
+		{
+			free_dpp(all_paths);
+			return (tmp);
+		}
 		free(tmp);
 		i++;
 	}
@@ -46,6 +57,7 @@ int	exec_external(t_app *app, char **cmd_args)
 	if (pid < 0)
 	{
 		ft_printf(RED "Error: fork failed\n" RST);
+		free(cmd_path);
 		return (1);
 	}
 	
@@ -54,11 +66,13 @@ int	exec_external(t_app *app, char **cmd_args)
 		execve(cmd_path, cmd_args, app->env);
 		// If execve returns, it means there was an error
 		ft_printf(RED "Error: execve failed\n" RST);
+		free(cmd_path);
 		exit(1); // Exit the child process
 	}
 	else  // Parent process
 	{
 		waitpid(pid, &status, 0);
+		free(cmd_path);
 		if (WIFEXITED(status))
 			return (WEXITSTATUS(status));
 		return (1); // Return 1 if the child process did not exit correctly
